8824e652fcb3a796a11eb10c7661613a8478b8e1
Joshua Hampson <josh@zhbot.org>
2022-07-30 07:53:12 +0100

    Update OldPrimitiveGroupP3DChunk.lua
    * Added automatic `VertexType` calculation

--- lib/P3DChunks/OldPrimitiveGroupP3DChunk.lua
+++ lib/P3DChunks/OldPrimitiveGroupP3DChunk.lua
@@ -18,21 +18,19 @@ local table_unpack = table.unpack
 local assert = assert
 local type = type
 
-local function new(self, Version, ShaderName, PrimitiveType, VertexType, NumVertices, NumIndices, NumMatrices)
+local function new(self, Version, ShaderName, PrimitiveType, NumVertices, NumIndices, NumMatrices)
 	assert(type(Version) == "number", "Arg #1 (Version) must be a number.")
 	assert(type(ShaderName) == "string", "Arg #2 (ShaderName) must be a string.")
 	assert(type(PrimitiveType) == "number", "Arg #3 (PrimitiveType) must be a number.")
-	assert(type(VertexType) == "number", "Arg #4 (VertexType) must be a number.")
-	assert(type(NumVertices) == "number", "Arg #5 (NumVertices) must be a number.")
-	assert(type(NumIndices) == "number", "Arg #6 (NumIndices) must be a number.")
-	assert(type(NumMatrices) == "number", "Arg #7 (NumMatrices) must be a number.")
+	assert(type(NumVertices) == "number", "Arg #4 (NumVertices) must be a number.")
+	assert(type(NumIndices) == "number", "Arg #5 (NumIndices) must be a number.")
+	assert(type(NumMatrices) == "number", "Arg #6 (NumMatrices) must be a number.")
 
 	local Data = {
 		Chunks = {},
 		Version = {},
 		ShaderName = {},
 		PrimitiveType = {},
-		VertexType = {},
 		NumVertices = {},
 		NumIndices = {},
 		NumMatrices = {},
@@ -51,22 +49,73 @@ P3D.OldPrimitiveGroupP3DChunk.PrimitiveTypes = {
 	LineStrip = 3
 }
 P3D.OldPrimitiveGroupP3DChunk.VertexTypes = {
-	UVs = 1 << 0,
-	UVs2 = 1 << 1,
-	UVs3 = 1 << 2,
-	UVs4 = 1 << 3,
+	UVs = 1,
+	UVs2 = 2,
+	UVs3 = 3,
+	UVs4 = 4,
+	UVs5 = 5,
+	UVs6 = 6,
+	UVs7 = 7,
+	UVs8 = 8,
 	Normals = 1 << 4,
 	Colours = 1 << 5,
-	Matrices = 1 << 6,
-	Weights = 1 << 7,
-	Unknown = 1 << 8,
+	Specular = 1 << 6,
+	Matrices = 1 << 7,
+	Weights = 1 << 8,
+	Position = 1 << 13,
 }
 function P3D.OldPrimitiveGroupP3DChunk:parse(Contents, Pos, DataLength)
 	local chunk = self.parentClass.parse(self, Contents, Pos, DataLength, self.Identifier)
 	
-	chunk.Version, chunk.ShaderName, chunk.PrimitiveType, chunk.VertexType, chunk.NumVertices, chunk.NumIndices, chunk.NumMatrices = string_unpack("<Is1IIIII", chunk.ValueStr)
+	local num
+	chunk.Version, chunk.ShaderName, chunk.PrimitiveType, num, chunk.NumVertices, chunk.NumIndices, chunk.NumMatrices = string_unpack("<Is1IIIII", chunk.ValueStr)
 
 	return chunk
+end
+
+function P3D.OldPrimitiveGroupP3DChunk:GetVertexType()
+	local vertexType = 0
+	
+	local uvN = 0
+	for i=1,#self.Chunks do
+		local identifier = self.Chunks[i].Identifier
+		if identifier == P3D.Identifiers.UV_List then
+			uvN = uvN + 1
+		elseif identifier == P3D.Identifiers.Packed_Normal_List or identifier == P3D.Identifiers.Normal_List then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.Normals
+		elseif identifier == P3D.Identifiers.Colour_List then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.Colours
+		elseif identifier == P3D.Identifiers.Matrix_List or identifier == P3D.Identifiers.Matrix_Palette then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.Matrices
+		elseif identifier == P3D.Identifiers.Weight_List then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.Weights
+		elseif identifier == P3D.Identifiers.Position_List then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.Position
+		end
+	end
+	if uvN > 0 then
+		if uvN == 1 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs
+		elseif uvN == 2 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs2
+		elseif uvN == 3 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs3
+		elseif uvN == 4 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs4
+		elseif uvN == 5 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs5
+		elseif uvN == 6 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs6
+		elseif uvN == 7 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs7
+		elseif uvN == 8 then
+			vertexType = vertexType | P3D.OldPrimitiveGroupP3DChunk.VertexTypes.UVs8
+		else
+			error("Too manu UVs")
+		end
+	end
+	
+	return vertexType
 end
 
 function P3D.OldPrimitiveGroupP3DChunk:__tostring()
@@ -79,5 +128,5 @@ function P3D.OldPrimitiveGroupP3DChunk:__tostring()
 	local ShaderName = P3D.MakeP3DString(self.ShaderName)
 	
 	local headerLen = 12 + 4 + #ShaderName + 1 + 4 + 4 + 4 + 4 + 4
-	return string_pack("<IIIIs1IIIII", self.Identifier, headerLen, headerLen + #chunkData, self.Version, ShaderName, self.PrimitiveType, self.VertexType, self.NumVertices, self.NumIndices, self.NumMatrices) .. chunkData
+	return string_pack("<IIIIs1IIIII", self.Identifier, headerLen, headerLen + #chunkData, self.Version, ShaderName, self.PrimitiveType, self:GetVertexType(), self.NumVertices, self.NumIndices, self.NumMatrices) .. chunkData
 end
