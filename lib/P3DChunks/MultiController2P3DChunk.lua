efbc31e1628b5ae8ac735abe92a28daae173e843
Proddy <josh@zhbot.org>
2024-08-12 13:19:48 +0100

    v2.3 - Add `NumTracks` calculation to `MultiControllerTrack2`

--- lib/P3DChunks/MultiController2P3DChunk.lua
+++ lib/P3DChunks/MultiController2P3DChunk.lua
@@ -41,7 +41,6 @@ local function new(self, Version, Name, CycleMode, NumCycles, InfiniteCycle, Num
 		InfiniteCycle = InfiniteCycle,
 		NumFrames = NumFrames,
 		FrameRate = FrameRate,
-		NumTracks = NumTracks,
 	}
 	
 	self.__index = self
@@ -53,13 +52,23 @@ P3D.MultiController2P3DChunk.new = new
 function P3D.MultiController2P3DChunk:parse(Endian, Contents, Pos, DataLength)
 	local chunk = self.parentClass.parse(self, Endian, Contents, Pos, DataLength, self.Identifier)
 	
-	chunk.Version, chunk.Name, chunk.CycleMode, chunk.NumCycles, chunk.InfiniteCycle, chunk.NumFrames, chunk.FrameRate, chunk.NumTracks = string_unpack(Endian .. "Is1c4IIffI", chunk.ValueStr)
+	chunk.Version, chunk.Name, chunk.CycleMode, chunk.NumCycles, chunk.InfiniteCycle, chunk.NumFrames, chunk.FrameRate = string_unpack(Endian .. "Is1c4IIff", chunk.ValueStr)
 	chunk.Name = P3D.CleanP3DString(chunk.Name)
 	if Endian == ">" then
 		chunk.CycleMode = string_reverse(chunk.CycleMode)
 	end
 
 	return chunk
+end
+
+function P3D.MultiController2P3DChunk:GetNumTracks()
+	local n = 0
+	for i=1,#self.Chunks do
+		if self.Chunks[i].Identifier == P3D.Identifiers.Multi_Controller_Track then
+			n = n + 1
+		end
+	end
+	return n
 end
 
 function P3D.MultiController2P3DChunk:__tostring()
@@ -76,5 +85,5 @@ function P3D.MultiController2P3DChunk:__tostring()
 	end
 	
 	local headerLen = 12 + 4 + #Name + 1 + 4 + 4 + 4 + 4 + 4 + 4
-	return string_pack(self.Endian .. "IIIIs1c4IIffI", self.Identifier, headerLen, headerLen + #chunkData, self.Version, Name, CycleMode, self.NumCycles, self.InfiniteCycle, self.NumFrames, self.FrameRate, self.NumTracks) .. chunkData
+	return string_pack(self.Endian .. "IIIIs1c4IIffI", self.Identifier, headerLen, headerLen + #chunkData, self.Version, Name, CycleMode, self.NumCycles, self.InfiniteCycle, self.NumFrames, self.FrameRate, self:GetNumTracks()) .. chunkData
 end
